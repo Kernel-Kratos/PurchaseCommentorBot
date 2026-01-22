1164b6b4c30a018a954fd9c28a07b32e1b2b70b6
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-01-22 22:09:56 +0530

    Add basic file structure

---
+++ purschaseCommentor/src/main/java/com/discordBot/purschaseCommentor/PurschaseCommentorApplication.java
@@ -0,0 +1,13 @@
+package com.discordBot.purschaseCommentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurschaseCommentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurschaseCommentorApplication.class, args);
+	}
+
+}
---
+++ purschaseCommentor/src/test/java/com/discordBot/purschaseCommentor/PurschaseCommentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discordBot.purschaseCommentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurschaseCommentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
