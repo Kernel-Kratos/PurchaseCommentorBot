5708cdc3a6a5cf0bfa26bbc8bc025c71bedf3ea7
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2026-02-08 14:43:13 +0530

    Updated

--- purschaseCommentor/src/main/java/com/discordBot/purschaseCommentor/PurschaseCommentorApplication.java
+++ purschaseCommentor/src/main/java/com/discordBot/purschaseCommentor/PurschaseCommentorApplication.java
@@ -1,13 +1,11 @@
 package com.discordBot.purschaseCommentor;
-
+	
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PurschaseCommentorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PurschaseCommentorApplication.class, args);
 	}
-
 }
